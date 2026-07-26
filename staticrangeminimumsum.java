490c7b88677e64200a5b5ea3975221579ae195c8
Dhanush N <mail2dhanushn@gmail.com>
2026-07-26 19:45:07 +0530

    Add files via upload

---
+++ staticrangeminimumsum.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class staticrangeminimumsum {
+//    https://cses.fi/problemset/task/1647
+static void main() throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter out = new PrintWriter(System.out);
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int q = Integer.parseInt(st.nextToken());
+    for(int i=0;i<n;i++){
+        long val=Long.parseLong(st.nextToken());
+    }
+
+}
+}
+
